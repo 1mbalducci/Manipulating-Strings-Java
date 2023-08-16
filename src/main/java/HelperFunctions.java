bb95184e7c87f52198141ff2cce91d569651e8eb
Michelle Balducci <michelle.balducci@gmail.com>
2023-08-16 09:45:08 -0500

    Reversing a string-Understood

---
+++ src/main/java/HelperFunctions.java
@@ -0,0 +1,59 @@
+public class HelperFunctions {
+
+    // Reverse the letters
+    // of the word
+    static void reverse(char str[], int start, int end)
+    {
+        // Temporary variable
+        // to store character
+        char temp;
+
+        while (start <= end) {
+            // Swapping the first
+            // and last character
+            temp = str[start];
+            str[start] = str[end];
+            str[end] = temp;
+            start++;
+            end--;
+        }
+    }
+    // Function to reverse words
+    static char[] reverseWords(char[] s)
+    {
+        // Reversing individual words as
+        // explained in the first step
+
+        int start = 0;
+        for (int end = 0; end < s.length; end++) {
+            // If we see a space, we
+            // reverse the previous
+            // word (word between
+            // the indexes start and end-1
+            // i.e., s[start..end-1]
+            if (s[end] == ' ') {
+                System.out.println("start is:" + start);
+                System.out.println("end is:" + end);
+                System.out.println(s);
+                reverse(s, start, end);
+                System.out.println("start is this AFTER the reverse:" + start);
+                System.out.println("end is this AFTER the reverse:" + end);
+                System.out.println(s);
+
+                start = end + 1;
+                System.out.println("start is this after the add:" + start);
+            }
+        }
+        System.out.println("__________________");
+
+
+        // Reverse the entire String
+        System.out.println(s);
+        reverse(s, 0, s.length - 1);
+        System.out.println(s);
+        System.out.println("0000000000000");
+        return s;
+
+
+    }
+}
--- src/main/java/ReverseTheString.java
+++ src/main/java/ReverseTheString.java
@@ -1,5 +1,46 @@
+import java.util.Arrays;
+
 public class ReverseTheString {
     public static void main(String[] args){
+        String s = "I like this program very much ";
+        char [] charArray= s.toCharArray();
+        // Function call
+        char[] p = HelperFunctions.reverseWords(charArray);
+        System.out.print(p);
+
+        System.out.print("________________________");
+
+        String temp;
+        String word="babys are small people";
+        String[] babyChar= word.split(" ");
+        int start =0;
+        int end=babyChar.length-1;
+
+        while (start <= end) {
+            // Swapping the first
+            // and last character
+            temp = babyChar[start];
+            babyChar[start] = babyChar[end];
+            babyChar[end] = temp;
+            System.out.println(Arrays.toString(babyChar));
+            start++;
+            end--;
+        }
+
+        StringBuilder newString= new StringBuilder();
+
+        for (String theWord :babyChar){
+            newString.append(theWord).append(" ");
+        }
+
+        System.out.println(newString);
+
+
+
+
+
+
+
 
 
 
@@ -9,3 +50,10 @@ public class ReverseTheString {
 
     }
 }
+
+
+
+
+
+
+
